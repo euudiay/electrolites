7c36a611119dca058ef1699d2e71aa37fa14561b
mshobj <mshobj@68977649-22b1-bdde-d9ff-5ac616e2ec34>
2011-10-14 09:17:49 +0000

    Whatever.

--- trunk/src/com/electrolites/ecg/AnimationView.java
+++ trunk/src/com/electrolites/ecg/AnimationView.java
@@ -1,9 +1,13 @@
-package com.electrolites.ecg;
+package droid.hellow;
 
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathDashPathEffect;
+import android.graphics.Point;
+import android.graphics.RectF;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.util.AttributeSet;
@@ -20,7 +24,7 @@ public class AnimationView extends SurfaceView implements SurfaceHolder.Callback
 		
 		private SurfaceHolder _surfaceHolder;
 		
-		private Paint textPaint, linePaint;
+		private Paint textPaint, linePaint, linePaint2;
 		
 		public AnimationThread(SurfaceHolder holder) {
 			_surfaceHolder = holder;
@@ -30,8 +34,12 @@ public class AnimationView extends SurfaceView implements SurfaceHolder.Callback
 			textPaint.setTextSize(32.f);
 			
 			linePaint = new Paint();
-			linePaint.setARGB(100, 255, 100, 200);
-			linePaint.setStrokeWidth(3.f);
+			linePaint.setARGB(200, 100, 255, 100);
+			linePaint.setStrokeWidth(2.f);
+			
+			linePaint2 = new Paint();
+			linePaint2.setARGB(200, 255, 255, 255);
+			linePaint2.setStrokeWidth(1.f);
 		}
 		
 		public void run() {
@@ -70,14 +78,108 @@ public class AnimationView extends SurfaceView implements SurfaceHolder.Callback
 			_lastTime = now;
 		}
 		
+		
 		private void onRender(Canvas canvas) {
 			canvas.drawColor(Color.BLACK);
-			canvas.drawText("fps: " + fps, 0, 0, textPaint);
+			canvas.drawText("fps: " + fps, 100, 100, textPaint);
 			
-			canvas.drawLine(0, getHeight()/2, getWidth()/4, getHeight()/2, linePaint);
-			canvas.drawLine(getWidth()/4, getHeight()/2, getWidth()/2, getHeight()/4, linePaint);
-			canvas.drawLine(getWidth()/2, getHeight()/4, 3*getWidth()/4, getHeight()/2, linePaint);
-			canvas.drawLine(3*getWidth()/4, getHeight()/2, getWidth(), getHeight()/2, linePaint);
+			//canvas.drawLine(0, getHeight()/2, getWidth()/4, getHeight()/2, linePaint);
+			//canvas.drawLine(getWidth()/4, getHeight()/2, getWidth()/2, getHeight()/4, linePaint);
+			//canvas.drawLine(getWidth()/2, getHeight()/4, 3*getWidth()/4, getHeight()/2, linePaint);
+			//canvas.drawLine(3*getWidth()/4, getHeight()/2, getWidth(), getHeight()/2, linePaint);
+			
+			Path cosa = new Path();
+			cosa.addArc(new RectF(0, 0, getWidth(), getHeight()), 90, 90);
+			//canvas.drawPath(cosa, linePaint);
+			
+			float w = getWidth();
+			float h = getHeight();
+			
+			float[] points = {
+				0, h / 2,
+				w / 16, h / 2,
+				
+				w / 16, h / 2,
+				w / 8, 2*h / 5,
+				
+				w / 8, 2*h / 5,
+				3*w / 16, h / 2,
+				
+				3*w / 16, h / 2,
+				w / 4, h / 2,
+				
+				w / 4, h / 2,
+				5*w / 16, 3*h / 5,
+				
+				5*w / 16, 3*h / 5,
+				3*w / 8, h / 2,
+				
+				3*w / 8, h / 2,
+				7*w / 16, h / 10,
+				
+				7*w / 16, h / 10,
+				w / 2, 4*h / 5,
+				
+				w / 2, 4*h / 5,
+				9*w / 16, h / 2,
+				
+				9*w / 16, h / 2,
+				11*w / 16, h / 2,
+				
+				11*w / 16, h / 2,
+				3*w / 4, 3*h / 10,
+				
+				3*w / 4, 3*h / 10,
+				13*w / 16, h / 2,
+				
+				13*w / 16, h / 2,
+				w, h /2
+			};
+			
+			float[] points2 = {
+					0, h / 2 - 1,
+					w / 16, h / 2 - 1,
+					
+					w / 16, h / 2 - 1,
+					w / 8, 2*h / 5 - 1,
+					
+					w / 8, 2*h / 5 - 1,
+					3*w / 16, h / 2 - 1,
+					
+					3*w / 16, h / 2 - 1,
+					w / 4, h / 2 - 1,
+					
+					w / 4, h / 2 - 1,
+					5*w / 16, 3*h / 5 - 1,
+					
+					5*w / 16, 3*h / 5 - 1,
+					3*w / 8, h / 2 - 1,
+					
+					3*w / 8, h / 2 - 1,
+					7*w / 16, h / 10 - 1,
+					
+					7*w / 16, h / 10 - 1,
+					w / 2, 4*h / 5 - 1,
+					
+					w / 2, 4*h / 5 - 1,
+					9*w / 16, h / 2 - 1,
+					
+					9*w / 16, h / 2 - 1,
+					11*w / 16, h / 2 - 1,
+					
+					11*w / 16, h / 2 - 1,
+					3*w / 4, 3*h / 10 - 1,
+					
+					3*w / 4, 3*h / 10 - 1,
+					13*w / 16, h / 2 - 1,
+					
+					13*w / 16, h / 2 - 1,
+					w, h /2 - 1
+				};
+			
+			canvas.drawLines(points, linePaint);
+			canvas.drawLines(points2, linePaint2);
+			canvas.drawTextOnPath("Pferv es un chico apuesto", cosa, 2.f, 2.f, linePaint);
 			
 			canvas.restore();
 		}
