6cb50a4224046cbaf0e55651f4e543deb1c721ca
mshobj <mshobj@68977649-22b1-bdde-d9ff-5ac616e2ec34>
2011-10-16 14:04:15 +0000

    - He hecho AnimationView y AnimationThread abstractas, para que podamos heredar de ellas y reaprovechar Views con control de fps que nos gusta tanto (es una idea que se me ha ocurrido, no sé si os gustará). - Añadida HeartRateView, que muestra el ritmo cardíaco del paciente (de manera random de momento).

--- gen/com/electrolites/ecg/R.java
+++ gen/com/electrolites/ecg/R.java
@@ -14,10 +14,12 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int animationView1=0x7f050002;
-        public static final int button1=0x7f050001;
-        public static final int button2=0x7f050003;
+        public static final int button1=0x7f050002;
+        public static final int button2=0x7f050004;
+        public static final int eCGView1=0x7f050005;
+        public static final int heartRateView1=0x7f050003;
         public static final int linearLayout1=0x7f050000;
+        public static final int linearLayout2=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
---
+++ src/com/electrolites/ecg/AnimationThread.java
@@ -0,0 +1,65 @@
+package com.electrolites.ecg;
+
+import android.graphics.Canvas;
+import android.view.SurfaceHolder;
+
+public abstract class AnimationThread extends Thread {
+	private boolean _run;
+	private long _lastTime;
+	
+	private int frameSamplesCollected = 0;
+	private int frameSampleTime = 0;
+	protected int fps = 0;
+	protected int count = 0;
+	
+	private SurfaceHolder _surfaceHolder;
+
+	public AnimationThread(SurfaceHolder holder) {
+		_surfaceHolder = holder;
+	}
+	
+	public int getFPS() { return fps; }
+	
+	public void run() {
+		while (_run) {
+			Canvas c = null;
+			try {
+				c = _surfaceHolder.lockCanvas(null);
+				synchronized (_surfaceHolder) {
+					onUpdate();
+					onRender(c);
+				}
+			} finally {
+				if (c != null) {
+					_surfaceHolder.unlockCanvasAndPost(c);
+				}
+			}
+		}
+	}
+	
+	private void onUpdate() {
+		long now = System.currentTimeMillis();
+		
+		if (_lastTime != 0) {
+			int time = (int) (now - _lastTime);
+			frameSampleTime += time;
+			frameSamplesCollected++;
+			
+			if (frameSamplesCollected == 10) {
+				fps = (int) (10000 / frameSampleTime);
+				
+				frameSampleTime = 0;
+				frameSamplesCollected = 0;
+			}
+		}
+		
+		_lastTime = now;
+	}
+	
+	public abstract void onRender(Canvas canvas);
+	
+	public void setRunning(boolean b) {
+		_run = b;
+	}	
+}
+
