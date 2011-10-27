6d445c4cfeec14d3b39f05958115a062c50f7776
mshobj <mshobj@68977649-22b1-bdde-d9ff-5ac616e2ec34>
2011-10-27 09:31:16 +0000

    - Modificados los tipos de datos de Data - Métodos de parsing terminados (primera versión) - Reorganizadas las clases DataParser y FileConverter, que estaban regular. - Ahora el resource 'traza' se carga por su id sin harcodear.

--- src/com/electrolites/data/DPoint.java
+++ src/com/electrolites/data/DPoint.java
@@ -3,8 +3,8 @@ package com.electrolites.data;
 // Objeto que representa un punto resultante de la delineación
 public class DPoint {
 	// Tipo de punto: ninguno, comienzo, pico, final o pico secundario
-	enum PointType { none, start, peak, end, s_peak };
-	enum Wave { none, QRS, P, T };
+	public enum PointType { none, start, peak, end, s_peak };
+	public enum Wave { none, QRS, P, T };
 	
 	private PointType type;
 	private Wave wave;
@@ -19,7 +19,38 @@ public class DPoint {
 		this.wave = wave;
 	}
 	
+	public PointType checkType(int i) {
+		PointType type;
+		
+		switch (i & 0xf0) {
+			case 1: type = PointType.start; break;
+			case 2: type = PointType.peak; break;
+			case 3: type = PointType.end; break;
+			case 4: type = PointType.s_peak; break;
+			default: type = PointType.none;
+		}
+		
+		return type;
+	}
+	
+	public Wave checkWave(int i) {
+		Wave wave;
+		
+		switch (i & 0x0f) {
+			case 1: wave = Wave.QRS; break;
+			case 2: wave = Wave.P; break;
+			case 3: wave = Wave.T; break;
+			default: wave = Wave.none;
+		}
+		
+		return wave;
+	}
+	
 	public PointType getType() { return type; }
 	
+	public void setType(PointType type) { this.type = type; }
+	
 	public Wave getWave() { return wave; }
+	
+	public void setWave(Wave wave) { this.wave = wave; }
 }
--- src/com/electrolites/util/DataParser.java
+++ src/com/electrolites/util/DataParser.java
@@ -5,133 +5,189 @@ import java.util.Iterator;
 
 import android.content.res.Resources;
 
+import com.electrolites.data.DPoint;
 import com.electrolites.data.Data;
 
 public class DataParser {
-	enum Code {none, offset, hbr, sample, point };
-	
 	private FileConverter fc;		
 	private Data data;				// Instancia de Data
 	private ArrayList<Byte> stream;	// Bytes leídos
 	private short[] samples;		// Valores de las muestras, indexadas por orden de llegada
-	private int pointer;			// Apunta al siguiente byte al ï¿½ltimo leï¿½do (en data)
+	private int p1, p2;				// Punteros al último byte consumido y al último producido
 	private int lastSample;			// ï¿½ltima muestra leï¿½da (nï¿½mero de orden)
-	private int lastHBR;			// ï¿½ltimo resultado de ritmo cardï¿½aco leï¿½do
-	private Code lastCode;			// Tipo del ï¿½ltimo byte leï¿½do
-	
-	private int bytesLeft;			// Bytes que quedan por leer del campo actual
+	private float lastHBR;			// ï¿½ltimo resultado de ritmo cardï¿½aco leï¿½do
 	
 	public DataParser() {
-		fc = new FileConverter();
-		data = Data.getInstance();
-		stream = new ArrayList<Byte>();
-		pointer = 0;
+		data = Data.getInstance();			// Accedemos a los datos de la aplicación
+		stream = new ArrayList<Byte>();		// Instanciamos el vector de datos raw
+		
+		// Inicialmente no tenemos datos
+		p1 = 0;
+		p2 = 0;
 		lastSample = 0;
 		lastHBR = 0;
-		lastCode = Code.none;
-		
-		bytesLeft = 0;
 	}
 	
+	// Obtiene datos para la aplicación de un archivo binario
 	public void loadBinaryFile(String fname) {
-		fc.readBinary(fname);
-		stream = fc.getStream();
-		pointer = 0;
+		fc = new FileConverter();		// Instancia el conversor de archivos		
+		stream = fc.readBinary(fname);	// Lee y almacena los datos del archivo
+		
+		// Inicializa los punteros de lectura y escritura
+		p1 = 0;							
+		p2 = stream.size();
+		
+		// Inicialmente no tenemos datos
 		lastSample = 0;
 		lastHBR = 0;
-		lastCode = Code.none;
 		
-		bytesLeft = 0;
+		// Procesa y guarda los datos en Data
+		readStream();
 	}
 	
-	public void loadResource(Resources resources) {
-		fc.readResources(resources);
-		stream = fc.getStream();
-		pointer = 0;
+	// Obtiene datos para la aplicación de un recurso interno
+	public void loadResource(Resources resources, int id) {
+		fc = new FileConverter();					// Instancia el conversor de archivos
+		stream = fc.readResources(resources, id);	// Carga el recurso con identificador id
+		
+		// Inicializa los punteros de lectura y de escritura
+		p1 = 0;
+		p2 = stream.size();
+		
+		// Inicialmente no tenemos datos
 		lastSample = 0;
 		lastHBR = 0;
-		lastCode = Code.none;
-		
-		bytesLeft = 0;
+
+		// Procesa y guarda los datos en Data
+		readStream();
 	}
+	
+	public boolean hasNext() {
+		return (p1 <= p2);
+	} 
+	
+	public void nextField() {
+		// Obtenemos el siguiente byte
+		int new_byte = ((int) stream.get(p1)) & 0xff;
+		// No adelantamos el puntero hasta ver si tenemos suficientes datos para leer
+		int data_amount = p2 - p1;	// Cantidad de bytes por leer (sin contar el delimitador)
+		
+		switch (new_byte) {
+			case 0xcc:					// Offset
+				if (data_amount >= 4) 	// Tenemos que leer 4 bytes
+					readOffset();		// Comprueba, además, si se han perdido muestras
+				break;
+			case 0xfb:					// HBR
+				if (data_amount >= 2)
+					readHBR(); 			// Lee el ritmo cardíaco actual y lo guarda en data
+				break;
+			case 0xda:					// Sample
+				if (data_amount >= 2)
+					readSample();		// Lee y almacena el valor de la muestra en data
+				break;
+			case 0xed:					// Point
+				if (data_amount >= 5)
+					; 	// Tratar puntos de delineación
+				break;
+			default:
+				System.err.println("Delimitador no reconocido: " + new_byte);
+		}
+	}
+	
+	// Procesa y guarda todos los datos que tiene disponibles
+	public void readStream() {
+		while (hasNext())
+			nextField();
+	}
+	
 	
 	// Devuelve el nï¿½mero de muestras que se ha saltado, -1 si error
 	// Coloca el ï¿½ndice en el siguiente byte a los 5 del offset
 	// Actualiza el HBR con el nuevo valor
-	public int goToNextOffset() {
-		int i = pointer;
-		boolean found = false;
+	public void readOffset() {
+		// Antes el valor menos significativo
+		int byte0 = ((int) stream.get(p1+1)) & 0xff;
+		int byte1 = ((int) stream.get(p1+2)) & 0xff;
+		int byte2 = ((int) stream.get(p1+3)) & 0xff;
+		int byte3 = ((int) stream.get(p1+4)) & 0xff;
 		
-		while (i < stream.size() && !found) {
-			found = (stream.get(i) == 0xcc);
-			i++;
+		// Calculamos el número de muestras que nos dice el offset
+		int nSamples = byte0 + 256*(byte1 + 256*(byte2 + 256*byte3));
+		
+		// Si no coincide con la última muestra leída, hemos perdido muestras
+		if (lastSample < nSamples) {
+			// Rellenamos los huecos vacíos de las muestras en data
+			for (int i = 0; i < nSamples - lastSample; i++)
+				data.samples.add(null);
+			// Actualizamos cuál fue la última muestra (perdida)
+			lastSample = nSamples;
 		}
 		
-		if (found) {
-			// Antes el valor menos significativo
-			int byte0 = ((int) stream.get(i)) & 0xff;
-			int byte1 = ((int) stream.get(i+1)) & 0xff;
-			int byte2 = ((int) stream.get(i+2)) & 0xff;
-			int byte3 = ((int) stream.get(i+3)) & 0xff;
-			
-			if (stream.get(i) == 0xfb) {
-				// Actualiza el HBR (primero el byte mï¿½s significativo)
-				byte0 = ((int) stream.get(i+4)) & 0xff;
-				byte1 = ((int) stream.get(i+5)) & 0xff;
-				lastHBR = 15000 / (byte0*255 + byte1);
-			}
-			
-			pointer = i+6;
-			
-			int sample = byte0 + 256*(byte1 + 256*(byte2 + 256*byte3));
-			return sample - lastSample;
-		}
-		else return -1;
+		System.err.println("Se han perdido " + (nSamples - lastSample) + " muestras");
+		
+		// Adelantamos el puntero de lectura 5 posiciones (delimitador + 4 bytes)
+		p1 += 5;
 	}
 	
-	public Short getNextSample() {
-		int i = pointer;
-		boolean found = false;
-		Short sample = 0;
+	public void readHBR() {
+		int byte0 = ((int) stream.get(p1+1)) & 0xff;
+		int byte1 = ((int) stream.get(p1+2)) & 0xff;
+		// Calcula el ritmo cardíaco (60*250/X)
+		lastHBR = 15000f / ((float) (byte0*255 + byte1));
+		// Guarda en data el valor del ritmo cardíaco en este momento (indexándolo según la última muestra recibida)
+		data.hbr.put(lastSample, (short) lastHBR);
 		
-		// A partir de la posición del puntero, buscamos la siguiente muestra
-		while (i < stream.size() && !found) {
-			found = (stream.get(i) == 0xda);
-			i++;
-		}
-		
-		// Si la hemos encontrado...
-		if (found) {
-			pointer = i+2;	// Avanzamos el puntero dos posiciones
-			lastSample++;	// Incrementamos la cantidad de muestras leídas
-			
-			// Calculamos el valor de la muestra
-			sample = byteToShort(stream.get(i), stream.get(i+1));
-			// Añadimos la muestra con su número de orden a la tabla de muestras
-			data.samples.put(lastSample, sample);
-			
-			return sample;
-		}
-			
-		else
-			return null;
+		// Adelantamos el puntero de lectura 3 posiciones (delimitador + 3 bytes)
+		p1 += 3;
 	}
 	
-	// De momento sólo para leer de archivo
-	// Filtra los datos de las muestras de los demás datos y los guarda en el vector samples
+	public void readSample() {
+		// Incrementamos la cantidad de muestras leídas
+		lastSample++;	
+		// Calculamos el valor de la muestra
+		short sample = byteToShort(stream.get(p1+1), stream.get(p1+2));
+		// Añadimos la muestra con su número de orden a la tabla de muestras
+		data.samples.add(sample);
+		// Adelantamos el puntero de lectura 3 posiciones
+		p1 += 3;
+	}
+	
+	public void readDPoint() {
+		DPoint dp = new DPoint();
+		
+		int byte0 = ((int) stream.get(p1+1)) & 0xff;
+		
+		// Comprobamos el tipo de punto
+		dp.setType(dp.checkType(byte0));
+		// Comprobamos la onda a la que se refiere
+		dp.setWave(dp.checkWave(byte0));
+		
+		// Vemos a qué muestra se refiere
+		int byte1 = ((int) stream.get(p1+2)) & 0xff;
+		int byte2 = ((int) stream.get(p1+3)) & 0xff;
+		int byte3 = ((int) stream.get(p1+4)) & 0xff;
+		int byte4 = ((int) stream.get(p1+5)) & 0xff;
+		int sample = byte1 + 256*(byte2 + 256*(byte3 + 256*byte4));
+		
+		// Añadimos el punto a la tabla de puntos de data
+		data.dpoints.put(sample, dp);
+		
+		// Adelantamos el puntero de lectura 6 posiciones (delimitador + 5 bytes)
+		p1 += 6;
+	}
+	
+	// Old!
 	public void extractSamples() {
-		Iterator<Byte> it = stream.iterator();
+		Iterator<Short> it = data.samples.iterator();
 	
 		samples = new short[5000];
 		
-		byte a, b;
+		short sample = 0;
 		int i = 0;
-		while (it.hasNext()) {
+		while (it.hasNext() && i < 5000) {
 			if ((it.next() & 0xff) == 0xda) {
-				// Supongamos que esto funciona siempre
-				a = it.next();
-				b = it.next();
-				samples[i] = byteToShort(a, b);
+				sample = it.next();
+				samples[i] = sample;
 				i++;
 			}
 		}
@@ -148,40 +204,8 @@ public class DataParser {
 		return (short) (i1*256 + i2);
 	}
 	
-	// Avanza una posiciï¿½n en data y devuelve el tipo del byte
-	public Code getNextByte() {
-		// Obtenemos el siguiente byte
-		int new_byte = ((int) stream.get(pointer)) & 0xff;
-		pointer++;	// Adelantamos el puntero una posición
-		
-		if (bytesLeft == 0) {
-			// Ya se han leído todos los bytes del campo anterior
-			switch (new_byte) {
-				case 0xcc:
-					lastCode = Code.offset;
-					bytesLeft = 4;
-					break;
-				case 0xfb:
-					lastCode = Code.hbr;
-					bytesLeft = 2;
-					break;
-				case 0xda:
-					lastCode = Code.sample;
-					bytesLeft = 2;
-					break;
-				case 0xed:
-					lastCode = Code.point;
-					bytesLeft = 5;
-					break;
-			}
-		}
-		else
-			bytesLeft--;	// Hemos leído un byte más de los que faltaban por leer
-				
-		return lastCode;
-	}
+	public short getLastHBR() { return (short) lastHBR; }
 	
-	public int getLastHBR() { return lastHBR; }
-	
+	// Old!
 	public short[] getSamples() { return samples; }
 }
