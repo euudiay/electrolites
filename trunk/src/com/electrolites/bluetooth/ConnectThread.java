3e28620329edeb06a48f79ee928003c8fc6042e4
mshobj <mshobj@68977649-22b1-bdde-d9ff-5ac616e2ec34>
2011-11-09 19:38:05 +0000

    Commit de "por si acaso": - Creado el paquete "bluetooth", para guardar las clases de los threads que intervienen en la comunicación. - Añadido el esqueleto del BluetoothService. Todavía no hace nada, como todo buen esqueleto. - Reescrito y adaptado el código (de los threads) que he estado usando estos días (parte mío y parte cortesía de Google xD)
    Más tarde o mañana termino la implementación de BluetoothService (en su mayor parte es refactorizar e implementar el comportamiento que queramos que tenga).

---
+++ trunk/src/com/electrolites/bluetooth/ConnectThread.java
@@ -0,0 +1,73 @@
+package com.electrolites.bluetooth;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+import com.electrolites.services.BluetoothService;
+
+// Obtiene el socket para enviar datos
+public class ConnectThread extends Thread {
+	private final BluetoothAdapter bA;
+	private final BluetoothService bS;
+	private final BluetoothSocket socket;
+    private final BluetoothDevice device;
+
+    public ConnectThread(BluetoothAdapter bA, BluetoothService bS, BluetoothDevice device, UUID uuid) {
+        this.bA = bA;
+        this.bS = bS;
+    	this.device = device;
+        BluetoothSocket tmp = null;
+
+        // Obtenemos el socket para la conexión con el dispositivo dado
+        try {
+        	tmp = device.createRfcommSocketToServiceRecord(uuid);
+        } catch (IOException e) {
+            Log.e(BluetoothService.TAG, "Ha fallado la creación del socket.", e);
+        }
+        
+        socket = tmp;
+    }
+
+    public void run() {
+        Log.i(BluetoothService.TAG, "ConnectThread comienza su actividad");
+
+        // Cancelamos el descubrimiento de nuevos dispositivos
+        if (bA.isDiscovering())
+        	bA.cancelDiscovery();
+
+        // Creamos una conexión con el socket
+        try {
+            socket.connect(); // Llamada bloqueante
+        } catch (IOException e) {
+            // Cerramos el socket
+            try {
+                socket.close();
+            } catch (IOException e2) {
+                Log.e(BluetoothService.TAG, "No se puede cerrar el socket.", e2);
+            }
+            bS.connectionFailed();
+            return;
+        }
+
+        // Reseteamos el thread porque hemos terminado
+        synchronized (bS) {
+            bS.setConnectThread(null);
+        }
+
+        // Arrancamos el connectedThread
+        bS.connected(socket, device);
+    }
+
+    public void cancel() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            Log.e(BluetoothService.TAG, "Ha fallado el cierre del socket.", e);
+        }
+    }
+}
---
+++ trunk/src/com/electrolites/bluetooth/ConnectedThread.java
@@ -0,0 +1,78 @@
+package com.electrolites.bluetooth;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import com.electrolites.services.BluetoothService;
+
+import android.bluetooth.BluetoothSocket;
+import android.util.Log;
+
+// Actúa como cliente, una vez conectado el socket envía datos (o los recibe)
+public class ConnectedThread extends Thread {
+	private final BluetoothSocket socket;
+    private final InputStream input;
+    private final OutputStream output;
+    
+    private BluetoothService bS;
+
+    public ConnectedThread(BluetoothService bS, BluetoothSocket socket, String socketType) {
+        Log.d(BluetoothService.TAG, "ConnectedThread creado.");
+        this.bS = bS;
+        this.socket = socket;
+        InputStream tmpIn = null;
+        OutputStream tmpOut = null;
+
+        // Obtenemos los streams de entrada y salida
+        try {
+            tmpIn = socket.getInputStream();
+            tmpOut = socket.getOutputStream();
+        } catch (IOException e) {
+            Log.e(BluetoothService.TAG, "No se han podido obtener los streams del socket.", e);
+        }
+
+        input = tmpIn;
+        output = tmpOut;
+    }
+    
+    // Recepción de datos
+    public void run() {
+        Log.i(BluetoothService.TAG, "ConnectedThread comienza su actividad.");
+        byte[] buffer = new byte[1024];
+        int bytes;
+
+        // Se mantiene a la escucha mientras esté conectado
+        while (true) {
+            try {
+                // Leemos del InputStream
+                bytes = input.read(buffer);
+         
+                // Hacemos cosas con los datos obtenidos, como guardarlos en data
+                bS.manageReceivedData(bytes, buffer);
+            } catch (IOException e) {
+                Log.e(BluetoothService.TAG, "La conexión se ha perdido.", e);
+                bS.connectionLost();
+                break;
+            }
+        }
+    }
+    
+    // Envío de datos al dispositivo
+    public void write(byte[] buffer) {
+        try {
+            output.write(buffer);
+        } catch (IOException e) {
+            Log.e(BluetoothService.TAG, "No se ha podido realizar el envío.", e);
+        }
+    }
+
+    public void cancel() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            Log.e(BluetoothService.TAG, "Ha fallado el cierre del socket.", e);
+        }
+    }
+
+}
