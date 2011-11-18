29c9ce867863128e2de3e549183d3c4e2ecf96f7
thegraffo <thegraffo@68977649-22b1-bdde-d9ff-5ac616e2ec34>
2011-11-18 11:36:41 +0000

    Comenzada la implementación de la vista en tiempo real con su correspondiente apartado en Data.

---
+++ src/com/electrolites/util/SamplePoint.java
@@ -0,0 +1,11 @@
+package com.electrolites.util;
+
+public class SamplePoint {
+	public int id;
+	public short sample;
+	
+	public SamplePoint(int id, short value) {
+		this.id = id;
+		this.sample = value;
+	}
+}
